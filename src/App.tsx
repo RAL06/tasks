b083fb29ec0f5e96cbc1d4b86a4729e978545b78
Richard Lomonaco <ralomona@udel.edu>
2026-03-08 14:17:31 -0400

    fixed component double half

--- src/App.tsx
+++ src/App.tsx
@@ -20,13 +20,13 @@ function App(): React.JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
-            <ChooseTeam></ChooseTeam>
+            {/* <ChooseTeam></ChooseTeam> */}
             <hr></hr>
-            <ColoredBox></ColoredBox>
+            {/* <ColoredBox></ColoredBox> */}
             <hr></hr>
-            <ShoveBox></ShoveBox>
+            {/* <ShoveBox></ShoveBox> */}
             <hr></hr>
             <Counter></Counter>
             <hr />
